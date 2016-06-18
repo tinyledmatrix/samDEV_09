f2b74482027e5e41bfb24f37f2f2016827579bb6
Alex <jabber5@web.de>
2016-06-18 17:02:04 +0200

    Update blink.c

--- Firmware/blink.c
+++ Firmware/blink.c
@@ -15,22 +15,22 @@ void init_TC1(void)
 	REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TC1_TC2;	
 	REG_PM_APBCMASK |= PM_APBCMASK_TC1;
 	REG_TC1_CTRLA |=TC_CTRLA_PRESCALER_DIV8;	// prescaler: 8
-	REG_TC1_INTENSET = TC_INTENSET_OVF;			// enable overflow interrupt
-	REG_TC1_CTRLA |= TC_CTRLA_ENABLE;			// enable TC1
+	REG_TC1_INTENSET = TC_INTENSET_OVF;		// enable overflow interrupt
+	REG_TC1_CTRLA |= TC_CTRLA_ENABLE;		// enable TC1
 	while(TC1->COUNT16.STATUS.bit.SYNCBUSY==1);	// wait on sync
-	NVIC_EnableIRQ(TC1_IRQn);					// enable TC1 interrupt in the nested interrupt controller
+	NVIC_EnableIRQ(TC1_IRQn);			// enable TC1 interrupt in the nested interrupt controller
 }
 
 void TC1_Handler()
 {
-	REG_PORT_OUTTGL0 = PORT_PA02;				// troggle PA02
-	REG_TC1_INTFLAG = TC_INTFLAG_OVF;			// reset interrupt flag - NEEDED HERE!
+	REG_PORT_OUTTGL0 = PORT_PA02;		// troggle PA02
+	REG_TC1_INTFLAG = TC_INTFLAG_OVF;	// reset interrupt flag - NEEDED HERE!
 }
 
 int main(void)
 {
     SystemInit();
 	init_TC1();
-	REG_PORT_DIRSET0 = PORT_PA02;				// PA02(on board LED)-> output
-    while (1);									// no CPU tasks
+	REG_PORT_DIRSET0 = PORT_PA02;		// PA02(on board LED)-> output
+    while (1);					// no CPU tasks
 }
